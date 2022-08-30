6d3a7f27747975b95358fd24d02cb833432b4309
H H Arvind <arvindhh99@gmail.com>
2022-08-30 21:47:29 +0530

    Update autompg.py

--- autompg.py
+++ autompg.py
@@ -31,9 +31,9 @@ if selected == "Mileage":
             st.markdown("Please enter the details") 
     
             def numeric():
-                        hp = st.numer_input('Horsepower',40,170,10,key='horsepower')
-                        wt = st.numer_input('weight in pounds',0,10000,2000,key='weight')
-                        acc = st.numer_input('Acceleration (No seconds to reach 60mph speed)',0,50,20,key='acceleration')
+                        hp = st.number_input('Horsepower',40,170,10,key='horsepower')
+                        wt = st.number_input('weight in pounds',0,10000,2000,key='weight')
+                        acc = st.number_input('Acceleration (No seconds to reach 60mph speed)',0,50,20,key='acceleration')
                         return hp,wt,acc
 
             horsepower,weight,acceleration = numeric()
